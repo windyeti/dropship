1b4cf074ba59b925eaa18d5804fac7c28fea102a
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-11-19 12:03:44 +0300

    folder aws for images

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "dropship"
 set :repo_url, "git@github.com:windyeti/dropship.git"
 set :deploy_to, "/var/www/dropship"
 append :linked_files, "config/database.yml", "config/master.key"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public", "storage", "public/image_aws"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public", "storage"
 set :format, :pretty
 set :log_level, :info
 
